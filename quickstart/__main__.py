cd08df65e8c4d46f1668258581d2071c32df99d2
Roman K <sgrade@users.noreply.github.com>
2020-10-26 12:37:43 +0100

    quickstart aws

---
+++ quickstart/__main__.py
@@ -0,0 +1,24 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+from pulumi_aws import s3
+
+# Create an AWS resource (S3 Bucket)
+bucket = s3.Bucket('my-bucket',
+        website=s3.BucketWebsiteArgs(
+            index_document="index.html",
+        ))
+
+# Export the name of the bucket
+pulumi.export('bucket_name', bucket.id)
+
+bucketObject = s3.BucketObject(
+    'index.html',
+    acl='public-read',
+    content_type='text/html',
+    bucket=bucket,
+    content=open('site/index.html').read(),
+)
+
+pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
+
