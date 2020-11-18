8e7bc0f65cc613c1fa63d42151afbfeb27414c71
Roman K <sgrade@users.noreply.github.com>
2020-11-18 15:04:21 +0100

    aws-sandbox

---
+++ aws/sandbox/__main__.py
@@ -0,0 +1,34 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+import pulumi_aws as aws
+
+size = 't2.micro'
+ami = aws.get_ami(most_recent="true",
+                  owners=["137112412989"],
+                  filters=[{"name": "name", "values": ["amzn-ami-hvm-*"]}])
+
+# https://github.com/pulumi/pulumi-aws/blob/master/examples/alb-new-py/__main__.py
+
+vpc = aws.ec2.Vpc("sandbox-vpc",
+                  cidr_block="10.0.0.0/16",
+                  enable_dns_hostnames=True,
+                  enable_dns_support=True)
+
+subnet = aws.ec2.Subnet("sandbox-subnet",
+                        vpc_id=vpc.id,
+                        cidr_block="10.0.1.0/24")
+
+group = aws.ec2.SecurityGroup("sandbox-security-group",
+                              description='Enable HTTP access',
+                              vpc_id=vpc.id,  # reference VPC from above
+                              ingress=[{'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0']}])
+
+server = aws.ec2.Instance('sandbox-server',
+                          instance_type=size,
+                          subnet_id=subnet.id,  # !!! Without subnet_id instance creation fails (VPCIdNotSpecified)
+                          vpc_security_group_ids=[group.id],    # reference security group from above
+                          ami=ami.id)
+
+pulumi.export('publicIp', server.public_ip)
+pulumi.export('publicHostName', server.public_dns)
